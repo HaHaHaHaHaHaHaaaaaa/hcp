64ed09e5dfe142c54b772c0ce663d557aecea5dc
wbl <651753175@qq.com>
2017-08-24 17:40:06 +0800

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  isChanged:boolean=true;
   constructor(public navCtrl: NavController) {
 
   }
